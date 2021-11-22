312941bc36bba0f210b8cc828f9dcfef98b7e89d
MattJCR <mateojcr@gmail.com>
2021-11-22 08:20:57 +0100

    Init

---
+++ src/main/java/com/springboot/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,34 @@
+package com.springboot.reactor.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner{
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Mateo","Pedro","Carlos", "Juan", "")
+				.doOnNext(n -> {
+					if(n.isEmpty()) {
+						throw new RuntimeException("Nombres no pueden ser vacios");
+					}
+						System.out.println(n);
+					
+					
+				});
+		nombres.subscribe(log::info, error -> log.error(error.getMessage()));
+		
+	}
+	
+
+}
